0a8903a9e2aece4fb9a7c32e11e2d423fcd8ba0b
aashishsatya <ankarathaashish@gmail.com>
2015-07-20 15:42:39 +0530

    First commit

---
+++ ClassifierDemo.py
@@ -0,0 +1,13 @@
+"""
+Created on Mon Jul 20 12:14:16 2015
+
+A file that demonstrates the program classifying hard ham mails. The ideal
+classification would result in the program classifying all mails as spam, but
+this does not happen in practice.
+
+@author: aashishsatya
+"""
+
+from TrainingSetsUtil import *
+
+hard_ham_path = 'data/hard_ham'
